3989f0f4ac11558dad2a0c5a332dd90fdad0f9ae
sreesaiogetiammini <sreesaiogetiammini@gmail.com>
2023-12-17 15:57:02 -0700

    added

--- components/project.tsx
+++ components/project.tsx
@@ -1,91 +1,75 @@
-import React from 'react'
-import ProjectDetails from './projectdetails'
+import React from "react";
+import ProjectDetails from "./projectdetails";
 
 const Project = () => {
   return (
-    <main className='w-full mb-16 flex flex-col items-center justify-center'>
+    <main className="w-full mb-16 flex flex-col items-center justify-center">
+      <div className="px-4 sm:px-6 lg:px-8 grid grid-cols-24 gap-4 sm:gap-8">
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3">
+          <ProjectDetails
+            title="Stremify Video Streaming Service"
+            type="Web Application"
+            githubLink="https://github.com/sreesaiogetiammini/CapstoneProject.git"
+            imageUrl="/stremify.png"
+            websitelink="https://sharestreamify.vercel.app/"
+            techstack="Node.js | Next.js | Tailwind CSS | TypeScript | MongoDb Cloud | Prisma |NextApi"
+            info="Developed a cross-platform video streaming platform where users can gift movies to Friends/Family with Next.js and React to ensure a consistent user experience on web and mobile interfaces. To efficiently manage user data, video content, and streaming metadata, I implemented Prisma as the database 
+                        ORM and utilized MongoDB for data storage. Additionally, a scalable backend with Node.js will enable the platform to handle increased user activity and content growth while maintaining optimal performance and reliability."
+          />
+        </div>
 
-    <div className='px-4 sm:px-6 lg:px-8 grid grid-cols-12 gap-4 sm:gap-8'>
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3">
+          <ProjectDetails
+            title="Paints Andriod App"
+            type="Android Application"
+            githubLink="https://github.com/sreesaiogetiammini/Paints"
+            videoUrl="/paintsdemo.mp4"
+            websitelink="https://github.com/sreesaiogetiammini/Paints"
+            techstack="Kotlin | Room | Jetpack Compose | Flutter "
+            info="A collaborative drawing app was developed using Kotlin, Custom ViewModel, Jetpack Compose, and Ktor Server. It offers diverse tools for drawing, a user-friendly interface, and thorough testing. The app is reliable and performs well, enhancing user satisfaction. It was rigorously tested using Unit Test cases, Instrumented Tests, and Automation Testing using Espresso."
+          />
+        </div>
 
-        <div className='col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3'>
-       
-                    <ProjectDetails
-                        title='Stremify Video Streaming Service'
-                        type='Web Application'
-                        githubLink='https://github.com/sreesaiogetiammini/CapstoneProject.git'
-                        imageUrl='/stremify.png'
-                        websitelink='https://sharestreamify.vercel.app/'
-                        techstack='Node.js | Next.js | Tailwind CSS | TypeScript | MongoDb Cloud | Prisma |NextApi'
-                        info='Developed a cross-platform video streaming platform where users can gift movies to Friends/Family with Next.js and React to ensure a consistent user experience on web and mobile interfaces. To efficiently manage user data, video content, and streaming metadata, I implemented Prisma as the database 
-                        ORM and utilized MongoDB for data storage. Additionally, a scalable backend with Node.js will enable the platform to handle increased user activity and content growth while maintaining optimal performance and reliability.'
-                    />
-                    
-            </div>
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3">
+          <ProjectDetails
+            title="A Personal portfolio Website"
+            type="Portfolio Website"
+            githubLink="https://web.physics.utah.edu/~deemyad/#"
+            imageUrl="/personalportfolio.png"
+            websitelink="https://web.physics.utah.edu/~deemyad/#"
+            techstack="Next.js | Tailwind CSS | TypeScript "
+            info="A professional portfolio website using Next JS,  and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive."
+          />
+        </div>
 
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3">
+          <ProjectDetails
+            title="A Simple portfolio Website"
+            type="Portfolio Website"
+            githubLink="https://web.physics.utah.edu/~deemyad/#"
+            imageUrl="/deemyadWebsite.png"
+            websitelink="https://web.physics.utah.edu/~deemyad/#"
+            techstack="HTML | CSS | JS"
+            info="A simple portfolio website was built using HTML , CSS and Javascript during my intial days into programming for the Professor from Department of Astronomy and Physics."
+          />
+        </div>
 
-            <div className='col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3'>
-       
-                    <ProjectDetails
-                        title='Paints Andriod App'
-                        type='Android Application'
-                        githubLink='https://github.com/sreesaiogetiammini/Paints'
-                        videoUrl='/paintsdemo.mp4'
-                        websitelink='https://github.com/sreesaiogetiammini/Paints'
-                        techstack='Kotlin | Room | Jetpack Compose | Flutter '
-                        info='A collaborative drawing app was developed using Kotlin, Custom ViewModel, Jetpack Compose, and Ktor Server. It offers diverse tools for drawing, a user-friendly interface, and thorough testing. The app is reliable and performs well, enhancing user satisfaction. It was rigorously tested using Unit Test cases, Instrumented Tests, and Automation Testing using Espresso.'
-                    />
-                    
-            </div>
-
-            <div className='col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3'>
-       
-                    <ProjectDetails
-                        title='A Personal portfolio Website'
-                        type='Portfolio Website'
-                        githubLink='https://web.physics.utah.edu/~deemyad/#'
-                        imageUrl='/personalportfolio.png'
-                        websitelink='https://web.physics.utah.edu/~deemyad/#'
-                        techstack='Next.js | Tailwind CSS | TypeScript '
-                        info='A professional portfolio website using Next JS,  and Styled-components. It has smooth page transitions, cool background effects, unique design and it is mobile responsive.'
-                    />
-                    
-            </div>
-
-            <div className='col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3'>
-       
-                    <ProjectDetails
-                        title='A Simple portfolio Website'
-                        type='Portfolio Website'
-                        githubLink='https://web.physics.utah.edu/~deemyad/#'
-                        imageUrl='/deemyadWebsite.png'
-                        websitelink='https://web.physics.utah.edu/~deemyad/#'
-                        techstack='HTML | CSS | JS'
-                        info='A simple portfolio website was built using HTML , CSS and Javascript during my intial days into programming for the Professor from Department of Astronomy and Physics.'
-                    />     
-            </div>
-
-            <div className='col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3'>
-       
-                    <ProjectDetails
-                        title='BlackJack Game With SFML(C++)'
-                        type='Fun Game'
-                        githubLink='https://github.com/sreesaiogetiammini/BlackJackGameUsingSFML'
-                        imageUrl='/gamescreen.png'
-                        websitelink='https://github.com/sreesaiogetiammini/BlackJackGameUsingSFML'
-                        techstack='C++ | SFML '
-                        info=' This collaborative game project was built in C++ SFML libraries to make interactive screens for the game. Players can play the game 
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 xl:col-span-3">
+          <ProjectDetails
+            title="BlackJack Game With SFML(C++)"
+            type="Fun Game"
+            githubLink="https://github.com/sreesaiogetiammini/BlackJackGameUsingSFML"
+            imageUrl="/gamescreen.png"
+            websitelink="https://github.com/sreesaiogetiammini/BlackJackGameUsingSFML"
+            techstack="C++ | SFML "
+            info=" This collaborative game project was built in C++ SFML libraries to make interactive screens for the game. Players can play the game 
                         with keyboard and mouse interactions.
-                      '
-                    />     
-            </div>
+                      "
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
 
-                
-            </div>
-        </main>
-        
-  )
-}
-
-export default Project
-
-
+export default Project;
--- components/projectdetails.tsx
+++ components/projectdetails.tsx
@@ -19,45 +19,38 @@ const ProjectDetails : React.FC<InputProps> = ({title,type,githubLink,websitelin
   return (
    
     <article className='w-full flex flex-col lg:flex-row items-center justify-between rounded-3xl border border-solid border-black bg-white shadow-2xl p-6 lg:p-12'>
-        <Link href={websitelink} target='_blank' className='w-1/2 cursor-pointer overflow-hidden rounded-lg '>
+
+    <Link href={websitelink} target='_blank' className='w-full lg:w-1/2 cursor-pointer overflow-hidden rounded-lg mb-6 lg:mb-0 lg:mr-6'>
         {imageUrl ? (
-        <img src={imageUrl} className="w-full h-auto hover:" alt={title} />
+            <img src={imageUrl} className="w-full h-auto hover:" alt={title} />
         ) : (
-         <video className="w-full h-auto" controls>
+            <video className="w-full h-auto" controls>
                 <source src={videoUrl} type="video/mp4" />
-                                 Your browser does not support the video tag.
+                Your browser does not support the video tag.
             </video>
         )}
+    </Link>
 
-
+    <div className='w-full lg:w-1/2 flex flex-col items-start justify-between'>
+        <span className='text-red-700 font-medium text-xl'>
+            {type}
+        </span>
+        <Link href={websitelink} target='_blank' className='hover:underline underline-offset-2'>
+            <h2 className='my-2 text-left text-2xl lg:text-4xl font-bold'>{title}</h2>
         </Link>
-       
-   
-        <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
-            <span className='text-red-700 font-medium  text-xl'> 
-                {type}
-            </span>
-            <Link href={websitelink} target='_blank' className='hover:underline underline-offset-2'>
-                <h2 className='my-2 w-full text-left text-4xl font-bold'>{title}</h2>
+        <p className='my-2 font-medium text-black'>{info}</p>
+        <p className='my-2 font-medium text-black'>{techstack}</p>
+        <div className='mt-2 flex items-center'>
+            <Link href={githubLink} target='_blank'>
+                <AiFillGithub size={35} className="min-w-16 min-h-16 rounded flex items-center justify-center" />
             </Link>
-            <p className='my-2  font-medium text-black '>{info}</p>
-            <p className='my-2  font-medium text-black '>{techstack}</p>
-            <div className=' mt-2 flex items-center'>
-                 <Link href={websitelink} target='_blank'>
-                    <AiFillGithub size={35} className="min-w-16 min-h-16 rounded flex items-center justify-center"/>
-                </Link>
-                <Link href={websitelink} target='_blank'>
-                  <button className="ml-4 rounded bg-black text-white p-2 px-6 text-lg font-semibold">Visit</button>
-                </Link>
-            </div>
+            <Link href={websitelink} target='_blank'>
+                <button className="ml-4 rounded bg-black text-white p-2 px-6 text-lg font-semibold">Visit</button>
+            </Link>
         </div>
-  
-       
+    </div>
+</article>
 
-        
-
-
-    </article>
 
   )
 }
