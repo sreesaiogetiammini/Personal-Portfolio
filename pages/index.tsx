dfd04c79ddcfbfa4e307a72503c17429f73d247f
sreesaiogetiammini <sreesaiogetiammini@gmail.com>
2024-02-13 15:21:07 -0700

    added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,18 @@ import { Inter } from "next/font/google";
 import Header from "../components/header";
 import HomePage from "../components/homepage";
 import Footer from "../components/footer";
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from "@vercel/analytics/react";
+import AboutPage from "@/components/aboutpage";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex flex-col h-screen lg:overflow-hidden">
-      <Analytics/>
-      <Header/>
-      <AboutPage/>
-      <div className='pt-8'>
-          <Footer/>
+      <Analytics />
+      <Header />
+      <AboutPage />
+      <div className="pt-8">
+        <Footer />
       </div>
     </main>
   );
