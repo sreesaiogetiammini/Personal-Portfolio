31a58f21861b962aa48f6857fad63b23c7c23b9d
sreesaiogetiammini <sreesaiogetiammini@gmail.com>
2024-04-23 20:32:13 -0600

    new update

--- components/homepage.tsx
+++ components/homepage.tsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 const HomePage = () => {
   return (
-    <div className='flex flex-col lg:flex-row items-center justify-between text-black w-full min-h-screen'>
+    <div className="flex flex-col lg:flex-row items-center justify-between text-black w-full min-h-screen">
+      <img
+        className="w-full lg:w-1/4 h-auto mb-4 lg:mb-0 lg:mt-6"
+        src="Logo.png"
+        alt="logo"
+      ></img>
 
-    <img className='w-full lg:w-1/4 h-auto mb-4 lg:mb-0 lg:mt-6' src='Logo.png' alt='logo'></img>
-
-    <div className='flex-grow lg:w-3/4 p-4 mt-6 lg:mt-0'>
-        <p className='flex items-center font-normal text-lg lg:text-xl'>
-            Transforming challenges into opportunities, I seamlessly transitioned from a meticulous QA engineer to a dynamic Full Stack Software Engineer, delivering comprehensive solutions that unite quality assurance and development expertise to elevate digital experiences.
+      <div className="flex-grow lg:w-3/4 p-4 mt-6 lg:mt-0">
+        <p className="flex items-center font-normal text-lg lg:text-xl">
+          Transforming challenges into opportunities, I seamlessly transitioned
+          from a meticulous QA engineer to a dynamic Full Stack Software
+          Engineer, delivering comprehensive solutions that unite quality
+          assurance and development expertise to elevate digital experiences.
         </p>
 
-        <div className='flex flex-col lg:flex-row items-start lg:items-center mt-4 lg:mt-2'>
-            <Link href='/SreeSai_Resume.pdf' target='_blank' className='bg-black text-white flex items-center font-bold p-2.5 px-6 rounded mt-2 lg:mt-0 lg:ml-4 hover:bg-white hover:text-black hover:border-solid hover:border hover:border-black'>
-                Resume
-            </Link>
-            <Link href='tel:+13854612547' target='_blank' className='bg-black text-white flex items-center font-bold p-2.5 px-6 rounded mt-2 lg:mt-0 lg:ml-4 hover:bg-white hover:text-black hover:border-solid hover:border hover:border-black'>
-                Contact Me
-            </Link>
+        <div className="flex flex-col lg:flex-row items-start lg:items-center mt-4 lg:mt-2">
+          <Link
+            href="/SreeSai_Resume.pdf"
+            target="_blank"
+            className="bg-black text-white flex items-center font-bold p-2.5 px-6 rounded mt-2 lg:mt-0 lg:ml-4 hover:bg-white hover:text-black hover:border-solid hover:border hover:border-black"
+          >
+            Resume
+          </Link>
+          <Link
+            href="tel:+13854612547"
+            target="_blank"
+            className="bg-black text-white flex items-center font-bold p-2.5 px-6 rounded mt-2 lg:mt-0 lg:ml-4 hover:bg-white hover:text-black hover:border-solid hover:border hover:border-black"
+          >
+            Contact Me
+          </Link>
         </div>
+      </div>
     </div>
+  );
+};
 
-</div>
-
-  )
-}
-
-export default HomePage
+export default HomePage;
